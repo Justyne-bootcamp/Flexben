cf75188b81883f8ea049fda182cf12056387bd78
charleezychang <charleezychang@gmail.com>
2022-07-31 23:13:55 +0800

    [merge] alex and charles

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,24 +1,41 @@
-require('dotenv').config()
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const loginService = require('../services/loginService')
 const jwt = require('jsonwebtoken')
+
+const USERS_TABLE = process.env.USERS_TABLE
+const CATEGORIES_TABLE = process.env.CATEGORIES_TABLE
+const CUTTOFFS_TABLE = process.env.CUTTOFFS_TABLE
+const SECRET_KEY = process.env.SECRET_KEY
 
 const login = async (req, res) => {
     try {
         let base64Encoding = req.headers.authorization.split(" ")[1];
         let credentials = Buffer.from(base64Encoding, "base64").toString().split(":")
-        const userInfo = await loginService.login(credentials[0])
+
+        const loginParams = {
+            TableName: USERS_TABLE,
+            Key: {
+                employeeId: credentials[0] // should be email for actual database
+            }
+        }
+        // console.log(loginParams);
+        const userInfo = await loginService.login(loginParams)
         if (userInfo != null && await bcrypt.compare(credentials[1], userInfo.password)) {
             // Successful login
             // JWT authorization, serializes email
+
             const user = {
-                "user": credentials[0],
-                "employee_id": userInfo.employee_id,
+                "email": credentials[0],
+                "employeeNumber": userInfo.employeeNumber,
+                "firstName": userInfo.firstName,
+                "lastName": userInfo.lastName,
+                "companyCode": userInfo.companyCode,
                 "role": userInfo.role
             }
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
+            // console.log(user);
+            const accessToken = jwt.sign(user, SECRET_KEY)
             res.json({
-                "message": "Welcome back, " + userInfo.firstname + " " + userInfo.lastname + " (" + userInfo.role + ")",
+                "message": "Welcome back, " + userInfo.firstName + " " + userInfo.lastName + " (" + userInfo.role + ")",
                 "accessToken": accessToken
             })
         }
@@ -31,24 +48,61 @@ const login = async (req, res) => {
     }
 }
 
+let categoryList = []
+let cutOffList = []
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     // 401 Status: Theres no token
     if (token == null) res.sendStatus(401)
     else {
-        jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+        jwt.verify(token, SECRET_KEY, async (err, user) => {
             // 403 Status: You have an invalid token
             if (err) res.sendStatus(403)
             else {
                 req.user = user
+                // Populates/initializes categoryList
+                if (categoryList.length == 0) {
+                    const categoryListParams = {
+                        TableName: CATEGORIES_TABLE,
+                    }
+                    categoryList = await loginService.getCategories(categoryListParams)
+                }
+                if (cutOffList.length == 0) {
+                    const cutOffListParams = {
+                        TableName: CUTTOFFS_TABLE,
+                        ScanIndexForward: false,
+                    }
+                    cutOffList = await loginService.getCutOffs(cutOffListParams)
+                }
                 next()
             }
         })
     }
 }
 
+const exportCategories = async () => {
+    return new Promise (function (resolve, _reject) {
+        resolve(categoryList)
+    })
+}
+
+const exportCutOffs = async () => {
+    return new Promise (function (resolve, _reject) {
+        resolve(cutOffList)
+    })
+}
+
+const exportLatestCutOffs = async () => {
+    return new Promise (function (resolve, _reject) {
+        resolve(cutOffList[0])
+    })
+}
+
 module.exports = {
     login,
-    authenticateToken
+    authenticateToken,
+    exportCategories,
+    exportCutOffs,
+    exportLatestCutOffs
 }
--- services/printService.js
+++ services/printService.js
@@ -1,89 +1,89 @@
-const path = require('path')
-const mysql = require("mysql")
-const fs = require('fs');
+// const path = require('path')
+// const mysql = require("mysql")
+// const fs = require('fs');
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// });
 
-connection.connect(error => {
-    if (error) throw error;
-});
+// connection.connect(error => {
+//     if (error) throw error;
+// });
 
-const downloadReimbursement = async (req, res) => {
-    const checkIfExists = async (employee_id, reimbursement_id) => {
-        let checkIfExistsQuery =
-            `
-        select concat(employees.lastname, ', ', employees.firstname) as employee_name, employees.employee_number, flex_reimbursement.date_submitted, flex_reimbursement.transaction_number, flex_reimbursement.total_reimbursement_amount, flex_reimbursement.status
-        from flex_reimbursement
-        left join employees on flex_reimbursement.employee_id=employees.employee_id
-        where flex_reimbursement.employee_id=?
-        and (flex_reimbursement.status='submitted' or flex_reimbursement.status='approved')
-        and flex_reimbursement.flex_reimbursement_id=?
-        `
-        return new Promise(function (resolve, reject) {
-            connection.query(checkIfExistsQuery, [employee_id, reimbursement_id], function (error, results) {
-                if (error) reject(new Error(error));
-                else {
-                    resolve(JSON.stringify(results))
-                }
-            })
-        })
-    }
+// const downloadReimbursement = async (req, res) => {
+//     const checkIfExists = async (employee_id, reimbursement_id) => {
+//         let checkIfExistsQuery =
+//             `
+//         select concat(employees.lastname, ', ', employees.firstname) as employee_name, employees.employee_number, flex_reimbursement.date_submitted, flex_reimbursement.transaction_number, flex_reimbursement.total_reimbursement_amount, flex_reimbursement.status
+//         from flex_reimbursement
+//         left join employees on flex_reimbursement.employee_id=employees.employee_id
+//         where flex_reimbursement.employee_id=?
+//         and (flex_reimbursement.status='submitted' or flex_reimbursement.status='approved')
+//         and flex_reimbursement.flex_reimbursement_id=?
+//         `
+//         return new Promise(function (resolve, reject) {
+//             connection.query(checkIfExistsQuery, [employee_id, reimbursement_id], function (error, results) {
+//                 if (error) reject(new Error(error));
+//                 else {
+//                     resolve(JSON.stringify(results))
+//                 }
+//             })
+//         })
+//     }
 
-    let ifExistsResult = JSON.parse(await checkIfExists(req.user.employee_id, req.params.reimbursement_id))
-    if (ifExistsResult.length == 0) {
-        res.status(200).send("No reimbursement found. Please recheck reimbursement id.")
-        return
-    }
+//     let ifExistsResult = JSON.parse(await checkIfExists(req.user.employee_id, req.params.reimbursement_id))
+//     if (ifExistsResult.length == 0) {
+//         res.status(200).send("No reimbursement found. Please recheck reimbursement id.")
+//         return
+//     }
 
-    const getItems = async (reimbursement_id) => {
-        let getItemsQuery =
-            `
-        select categories.name,  flex_reimbursement_details.or_number, flex_reimbursement_details.name_of_establishment, flex_reimbursement_details.tin_of_establishment, flex_reimbursement_details.amount, flex_reimbursement_details.status
-        from flex_reimbursement_details
-        left join categories on flex_reimbursement_details.category_id=categories.category_id
-        where flex_reimbursement_details.flex_reimbursement_id=?
-        `
-        return new Promise(function (resolve, reject) {
-            connection.query(getItemsQuery, [reimbursement_id], function (error, results) {
-                if (error) reject(new Error(error));
-                else {
-                    resolve(JSON.stringify(results))
-                }
-            })
-        })
-    }
+//     const getItems = async (reimbursement_id) => {
+//         let getItemsQuery =
+//             `
+//         select categories.name,  flex_reimbursement_details.or_number, flex_reimbursement_details.name_of_establishment, flex_reimbursement_details.tin_of_establishment, flex_reimbursement_details.amount, flex_reimbursement_details.status
+//         from flex_reimbursement_details
+//         left join categories on flex_reimbursement_details.category_id=categories.category_id
+//         where flex_reimbursement_details.flex_reimbursement_id=?
+//         `
+//         return new Promise(function (resolve, reject) {
+//             connection.query(getItemsQuery, [reimbursement_id], function (error, results) {
+//                 if (error) reject(new Error(error));
+//                 else {
+//                     resolve(JSON.stringify(results))
+//                 }
+//             })
+//         })
+//     }
 
-    let collection = ifExistsResult[0]
-    let items = JSON.parse(await getItems(req.params.reimbursement_id))
-    let transaction_txt =
-`Employee Name: ${collection.employee_name}
-Employee Number: ${collection.employee_number}
-Date Submitted: ${collection.date_submitted.split("T")[0]}
-Transaction Number: ${collection.transaction_number}
-Amount: Php ${collection.total_reimbursement_amount}
+//     let collection = ifExistsResult[0]
+//     let items = JSON.parse(await getItems(req.params.reimbursement_id))
+//     let transaction_txt =
+// `Employee Name: ${collection.employee_name}
+// Employee Number: ${collection.employee_number}
+// Date Submitted: ${collection.date_submitted.split("T")[0]}
+// Transaction Number: ${collection.transaction_number}
+// Amount: Php ${collection.total_reimbursement_amount}
 
-=== DETAILS ===
-    ${items.map(item => {
-        return (
-        `
-Category: ${item.name}
-OR Number: ${item.or_number}
-Name of Establishment: ${item.name_of_establishment}
-TIN of Establishment: ${item.tin_of_establishment}
-Amount: Php ${item.amount}
-Status: ${item.status}
-`)
-        }).join(' ')}
-    `
-    fs.writeFileSync('./services/reimbursement.txt', transaction_txt)
-    res.download(path.resolve('./services/reimbursement.txt'))
-}
+// === DETAILS ===
+//     ${items.map(item => {
+//         return (
+//         `
+// Category: ${item.name}
+// OR Number: ${item.or_number}
+// Name of Establishment: ${item.name_of_establishment}
+// TIN of Establishment: ${item.tin_of_establishment}
+// Amount: Php ${item.amount}
+// Status: ${item.status}
+// `)
+//         }).join(' ')}
+//     `
+//     fs.writeFileSync('./services/reimbursement.txt', transaction_txt)
+//     res.download(path.resolve('./services/reimbursement.txt'))
+// }
 
-module.exports = {
-    downloadReimbursement
-}
+// module.exports = {
+//     downloadReimbursement
+// }
