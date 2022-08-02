67c98000f2edf4f404dc2322c6a928b8a8420501
charleezychang <charleezychang@gmail.com>
2022-08-02 16:25:38 +0800

    [add] date validation for dateOfPurchase

--- controllers/reimburseController.js
+++ controllers/reimburseController.js
@@ -66,6 +66,12 @@ const addReimbursement = async (req, res) => {
         return
     }
 
+    // date validation
+    if (!isValidDate(reimbursementItemDetails.dateOfPurchase)) {
+        res.status(400).send("Invalid date or follow the date format: YYYY/MM/DD")
+        return
+    }
+
     // minimum amount validation
     if (reimbursementItemDetails.amount < MIN_REIMBURSABLE_AMOUNT) {
         res.status(400).send("Amount should be equal or greater than " + MIN_REIMBURSABLE_AMOUNT)
@@ -353,6 +359,28 @@ const getDateToday = () => {
     today = yyyy + '/' + mm + '/' + dd;
     return today
 }
+
+function isValidDate(dateString)
+{
+    // Parse the date parts to integers YYYY/MM/DD
+    var parts = dateString.split("/");
+    var day = parseInt(parts[2], 10);
+    var month = parseInt(parts[1], 10);
+    var year = parseInt(parts[0], 10);
+
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
+};
 
 //Justyne
 const removeReimbursement = async (req, res) => {
